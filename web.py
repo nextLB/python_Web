23f50130293e2040612e7cceca90af3e6a397e6a
next <364574103@qq.com>
2025-10-04 20:18:01 +0800

    V1

--- web.py
+++ web.py
@@ -16,7 +16,7 @@ def index():
 
     # Flask内部会自动打开这个文件，并读取内容，将内容给用户返回。
     # 默认: 去当前项目目录的templates文件夹中找
-    return render_template("./html_files/index.html")
+    return render_template("./h")
 
 
 
