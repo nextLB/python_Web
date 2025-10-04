d7361e9f84a8f7aa30328658b9baeb1b2f48bfb1
next <364574103@qq.com>
2025-10-04 20:16:19 +0800

    V1

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 
+# 基于python的Web项目学习
 
+## 创建虚拟环境
+    
+    conda create -n next_web python=3.11
+
+## 启动虚拟环境，安装依赖包
+
+    pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple flask
+
+    
 
 
 
--- web.py
+++ web.py
@@ -1,10 +1,28 @@
 
+from flask import Flask, render_template
+
+# 配置模板文件夹为html_files
+app = Flask(__name__, template_folder='html_files')
+
+
+# 创建了网址 /show/info 和 函数index 的对应关系
+# 以后用户在浏览器上访问 /show/info, 网站自动执行 index
+@app.route("/show/info")
+def index():
+    # return "中国联通"
+
+    # return """<h1> 中国 </h1>
+    #         <span style='color:red;'>联通</span>"""
+
+    # Flask内部会自动打开这个文件，并读取内容，将内容给用户返回。
+    # 默认: 去当前项目目录的templates文件夹中找
+    return render_template("./html_files/index.html")
 
 
 
 
-
-
+if __name__ == '__main__':
+    app.run()
 
 
 
