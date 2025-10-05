0d67ec650d97e6fd98396f2892beb9848eceb624
next <364574103@qq.com>
2025-10-05 23:38:45 +0800

    V1

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # 配置模板文件夹为html_files
 app = Flask(__name__, template_folder='html_files')
@@ -7,8 +7,14 @@ app = Flask(__name__, template_folder='html_files')
 
 # 创建了网址 /show/info 和 函数index 的对应关系
 # 以后用户在浏览器上访问 /show/info, 网站自动执行 index
-@app.route("/show/info")
+@app.route("/show/info", methods=['GET'])
 def index():
+
+    # 1. 接受用户通过GET形式发送过来的数据
+    print(request.args)
+    # 2.给用户再返回结果
+
+
     # return "中国联通"
 
     # return """<h1> 中国 </h1>
@@ -30,6 +36,13 @@ def get_news():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+
+
+@app.route("/new_register", methods=['GET'])
+def new_register():
+    return render_template("new_register.html")
+
 
 
 if __name__ == '__main__':
